400567b06b446c72276f931831ba051f0501c29a
BlueNova <belal.meshaal123@gmail.com>
2024-01-04 22:47:56 +0100

    Update PORT variable to use 3000 instead of 80

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
